48a44c63de41ae6c9197aaea20b44894028ffa10
Dima <gremlin51088@gmail.com>
2026-06-18 21:25:10 +0300

    fix: reset file input value on click to allow re-import

--- src/screens/ItemsScreen.js
+++ src/screens/ItemsScreen.js
@@ -391,6 +391,7 @@ export default function ItemsScreen() {
             type="file"
             accept=".xlsx,.xls"
             onChange={handleFileChange}
+            onClick={e => { e.target.value = ''; }}
             style={{
               position: 'absolute', top: 0, left: 0,
               width: '100%', height: '100%',
